45cc6b86f307425bffd450e98769c014ec46b8dd
Draw Terraform <example@draw.terraform>
2020-12-11 07:47:20 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
-provider "aws" {
-   region = "eu-west-1"
-}
-
-resource "aws_vpc" "91acfac6-bcea-464d-8f1e-1852612aa153" {
-   default_route_table_id = "" 
-   link = "" 
+   default_security_group_id = "" 
    owner_id = "" 
-   instance_tenancy = "" 
-   ipv6_association_id = "" 
-   arn = "" 
+   ipv6_cidr_block = "" 
+   enable_dns_hostnames = "" 
+   enable_dns_support = "" 
+   main_route_table_id = "" 
+   default_network_acl_id = "" 
 }
 
