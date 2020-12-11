d886af6bf19cd73eb15f22807b44e3559b07cd61
Draw Terraform <example@draw.terraform>
2020-12-11 07:53:03 -0600

    Update main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-   default_network_acl_id = "" 
-   default_route_table_id = "" 
-   arn = "" 
-   ipv6_cidr_block = "" 
-   enable_dns_hostnames = "" 
-   main_route_table_id = "" 
-   id = "" 
-}
-
