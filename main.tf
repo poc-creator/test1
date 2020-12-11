c9954b19e14e3221c33a7be8c7bfc93940edb231
Draw Terraform <example@draw.terraform>
2020-12-11 06:39:48 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "eu-west-1"
+   region = "eu-west-1"
 }
 
 resource "aws_vpc" "26ab4c5f-1335-4eed-a82d-d419a2c806e3" {
-ipv6_cidr_block = "" 
-instance_tenancy = "" 
-enable_dns_support = "" 
-default_route_table_id = "" 
-arn = "" 
-cidr_block = "10.0.0.0/16" 
+   instance_tenancy = "" 
+   main_route_table_id = "" 
+   arn = "" 
+   link = "" 
+   ipv6_cidr_block = "" 
+   enable_classiclink = "" 
 }
 
